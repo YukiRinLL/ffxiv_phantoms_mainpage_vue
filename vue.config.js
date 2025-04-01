64eedb1cb6d1b113689d7d4c7d39efc42325d702
YukiRinLL <944989026@qq.com>
2025-04-01 11:28:42 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/ffxiv_phantoms_mainpage_vue/'  // 替换为你的仓库名
+      : '/'
 })
