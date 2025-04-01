f3b3402d1dfb04d430e6cc2f5008e98962b2cd6b
YukiRinLL <944989026@qq.com>
2025-04-01 11:57:35 +0800

    modify port to 3000

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/ffxiv_phantoms_mainpage_vue/'  // 替换为你的仓库名
-      : '/'
+      : '/',
+  devServer: {
+    port: 3000,  // 设置开发服务器端口为 3000
+    open: true    // 可选：启动后自动打开浏览器
+  }
 })
