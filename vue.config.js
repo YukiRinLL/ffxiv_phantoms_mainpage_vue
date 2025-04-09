6064f83853de27da917a1bee4ae381e993921522
YukiRinLL <944989026@qq.com>
2025-04-09 11:33:05 +0800

    Revert "update publicPath"
    This reverts commit e811f78c53ea098f7256f2d105f6aa94302e78f4.

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,10 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: [],
 
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/ffxiv_phantoms_mainpage_vue/'  // 仓库名
-      : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //     ? '/ffxiv_phantoms_mainpage_vue/'  // 仓库名
+  //     : '/',
+  publicPath: '/',
 
   outputDir: 'dist', // 默认构建目录（需与 Render 的 Publish Directory 一致）
   filenameHashing: true, // 为资源添加哈希
