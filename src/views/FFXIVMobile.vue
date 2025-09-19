728ea0108cc540969465943545ff86bfbbe4dde6
YukiRinLL <944989026@qq.com>
2025-09-19 17:34:32 +0800

    add FFXIVMobile page

--- src/views/FFXIVMobile.vue
+++ src/views/FFXIVMobile.vue
@@ -3,33 +3,49 @@
     <header>
       <div class="container container_solid">
         <div class="title_wrapper">
-          <h1>The Great Outdoors</h1>
+          <h1>Phantoms</h1>
         </div>
       </div>
       <div class="container container_image" aria-hidden="true">
         <div class="title_wrapper">
-          <h1>The Great Outdoors</h1>
+          <h1>Phantoms</h1>
         </div>
       </div>
     </header>
     <section>
-      <h2>Lorem ipsum dolor sit amet.</h2>
+      <h1>最终幻想-水晶世界 FC & linkshell</h1>
+      <h2>基本信息</h2>
       <p>
-        Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <strong>服务器:</strong> 天幕魔导城<br>
+        <strong>部队名称:</strong> Phantoms<br>
+        <strong>部队后缀:</strong> ＜终焉＞<br>
+        <strong>部队规模:</strong> 35人 (上限)<br>
+        <strong>语言环境:</strong> 中文 (Chinese)<br>
+        <strong>活跃时间:</strong> 全天 (All day)<br>
+        <strong>联系方式:</strong> QQ:936746977<br>
+        <strong>申请方式:</strong> 请联系任一管理员<br>
+      </p>
+      <h2>About FC/CWLS</h2>
+      <p>
+        <strong>共同运营</strong><br>
+        组内讨论氛围良好, 有许多热心的艾欧泽亚冒险者。<br>
+        讨论话题多为FF14游戏玩法机制/剧情/世界观设定/游戏环境 等内容。<br>
+        部队房装潢设施齐全，公会相关设定精细， 部队箱各类宠物坐骑消耗品免费供给！<br>
+        可以一起体验各类副本攻略、宝物探索、出海垂钓等团队活动。<br>
+        萌新大佬都可以开诚布公，共同打造良好环境，一起建设一个更好的FC。<br>
       </p>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
-        sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-        suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-        ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-        nulla pariatur?
+        <strong>一起Happy</strong><br>
+        找朋友们一起玩!在FC聊天频道中问问“玩点什么？”,我当然会邀请你！<br>
+        FC不限制视频拍摄、直播、社交平台上的截屏投稿等共享手段，请自由享受。<br>
+      </p>
+      <p>
+        <strong>平和友善 相互尊重</strong><br>
+        玩游戏开心第一！<br>
+        在MMORPG里也请保持自己的独立性，互相尊重和换位思考,不要将自己的想法强加对方。<br>
+        请重视对方的想法，然后试着请求或者帮忙，<br>
+        了解对方是避免强迫的最好的行为，注意不要因为自我深信而让对方不愉快。<br>
+        在不强迫的游戏中开心同乐才是最好的。
       </p>
     </section>
   </div>
@@ -103,5 +119,27 @@ section {
   padding: 2em;
   margin: auto;
   max-width: 800px;
+  background: white;
+  position: relative;
+  z-index: 3;
+  text-align: left; /* 设置文本左对齐 */
+}
+
+section h1, section h2, section p {
+  text-align: left; /* 确保标题和段落也左对齐 */
+}
+
+section h1 {
+  font-size: 2em;
+  margin-bottom: 0.5em;
+}
+
+section h2 {
+  font-size: 1.5em;
+  margin-bottom: 0.5em;
+}
+
+section p {
+  margin-bottom: 1em;
 }
 </style>
