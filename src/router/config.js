447708af5de5c17e0229c55ed5e777993971f442
YukiRinLL <944989026@qq.com>
2025-12-18 18:07:06 +0800

    add legacy

--- src/router/config.js
+++ src/router/config.js
@@ -7,6 +7,7 @@ import EmailOperations from '@/views/EmailOperations.vue';
 import UserManagement from '@/views/UserManagement.vue';
 import MessageManagement from '@/views/MessageManagement.vue';
 import FileManagement from '@/views/FileManagement.vue';
+import LegacyView from '@/views/legacy/LegacyView.vue';
 
 export const routeConfig = [
     {
@@ -76,6 +77,87 @@ export const routeConfig = [
         meta: {
             title: 'Mobile',
             icon: 'mobile'
+        }
+    },
+    // Legacy pages
+    {
+        path: '/legacy/qqchat',
+        name: 'LegacyQQChat',
+        component: LegacyView,
+        props: { page: 'QQchat' },
+        meta: {
+            title: 'QQ Chat',
+            icon: 'comment'
+        }
+    },
+    {
+        path: '/legacy/recruitment',
+        name: 'LegacyRecruitment',
+        component: LegacyView,
+        props: { page: 'Recruitment' },
+        meta: {
+            title: 'Recruitment',
+            icon: 'users'
+        }
+    },
+    {
+        path: '/legacy/editable-table-mobile',
+        name: 'LegacyEditableTableMobile',
+        component: LegacyView,
+        props: { page: 'editableTable-mobile' },
+        meta: {
+            title: 'Editable Table Mobile',
+            icon: 'table'
+        }
+    },
+    {
+        path: '/legacy/get-monthly-stats',
+        name: 'LegacyGetMonthlyStats',
+        component: LegacyView,
+        props: { page: 'getMonthlyStats' },
+        meta: {
+            title: 'Monthly Stats',
+            icon: 'chart-line'
+        }
+    },
+    {
+        path: '/legacy/guild-member-dynamic',
+        name: 'LegacyGuildMemberDynamic',
+        component: LegacyView,
+        props: { page: 'guild-member-dynamic' },
+        meta: {
+            title: 'Guild Member Dynamic',
+            icon: 'user-friends'
+        }
+    },
+    {
+        path: '/legacy/guild-members',
+        name: 'LegacyGuildMembers',
+        component: LegacyView,
+        props: { page: 'guild-members' },
+        meta: {
+            title: 'Guild Members',
+            icon: 'user-group'
+        }
+    },
+    // {
+    //     path: '/legacy/profiles',
+    //     name: 'LegacyProfiles',
+    //     component: LegacyView,
+    //     props: { page: 'profiles' },
+    //     meta: {
+    //         title: 'Profiles',
+    //         icon: 'id-card'
+    //     }
+    // },
+    {
+        path: '/legacy/visitor-stats',
+        name: 'LegacyVisitorStats',
+        component: LegacyView,
+        props: { page: 'visitor-stats' },
+        meta: {
+            title: 'Visitor Stats',
+            icon: 'chart-bar'
         }
     }
 ];
